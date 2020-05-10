64a5cbc800c8e3df4b9b66699e751373dcbd3158
sahilv16 <vermasahil350@gmail.com>
2020-05-10 13:22:03 +0530

    Updates

---
+++ precache-manifest.ab8e82f62233902cc101a92090fe77ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c06878c29d44997bfa0",
+    "url": "/task1/static/css/main.47a32cbe.chunk.css"
+  },
+  {
+    "revision": "5c06878c29d44997bfa0",
+    "url": "/task1/static/js/main.5c06878c.chunk.js"
+  },
+  {
+    "revision": "b68c94cfcb0a2c90435e",
+    "url": "/task1/static/js/1.b68c94cf.chunk.js"
+  },
+  {
+    "revision": "413715136121a56ff8b9",
+    "url": "/task1/static/js/runtime~main.41371513.js"
+  },
+  {
+    "revision": "c5d85a87cad45c42f22a06f1935d04e7",
+    "url": "/task1/index.html"
+  }
+];
